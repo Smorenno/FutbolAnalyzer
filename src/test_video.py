c5dc25c3225a2f7dff87cf867de29a99b84b66d4
Sergio Moreno <sermoreno00@gmail.com>
2025-11-19 19:03:11 +0100

    video reading with cv2 and VideoCapture

---
+++ src/test_video.py
@@ -0,0 +1,5 @@
+from video_io import openVideo
+
+route = "data/input/video.mp4"
+capturador = openVideo(route)
+print(capturador)
---
+++ src/video_io.py
@@ -0,0 +1,26 @@
+import cv2 as cv2
+import os.path
+
+
+def openVideo(videoRoute):
+    if os.path.exists(videoRoute):
+        video=cv2.VideoCapture(videoRoute)
+        if not video.isOpened():
+            raise Exception("No se puede abir el archivo")
+        return video
+    else:
+        raise Exception(f"El archivo no existe: {videoRoute}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
